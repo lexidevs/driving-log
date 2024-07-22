6bc0235f855318b24ed9e6bf4a0b78ec991d9274
lexidevs <github@lexiroth.com>
2024-07-22 09:35:17 -0600

    fix: set default preferences

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
-import * as React from 'react';
-import App from './src';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import { PaperProvider, MD3DarkTheme } from "react-native-paper";
+import * as React from "react";
+import App from "./src";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Main() {
+    // Set default values for preferences
+    React.useEffect(() => {
+        AsyncStorage.getItem("preferences").then((preferences) => {
+            if (preferences === null) {
+                AsyncStorage.setItem(
+                    "preferences",
+                    JSON.stringify({
+                        showDriveTime: true,
+                        showDriveProgress: true,
+                        showNightDriveTime: true,
+                        showNightDriveProgress: true,
+
+                        requiredDriveTime: 3000,
+                        requiredNightDriveTime: 600,
+                    })
+                );
+            }
+        });
+    }, []);
+
     return (
         <SafeAreaProvider>
             <PaperProvider theme={MD3DarkTheme}>
